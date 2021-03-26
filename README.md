53c78573e8a3070b43e0e361dc13370fb8174b95
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2021-03-27 01:15:04 +0200

    Initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eureka-project
+1. Run in bash ./eureka-server.sh
+2. Run in bash ./eureka-client.sh
---
+++ eureka-server/src/test/java/com/mightyjava/eurekeserver/EurekeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mightyjava.eurekeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
